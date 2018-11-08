4c2f1d1444eed34727fb05b1ca45502e95885876
student <student@imac-28.local>
2018-11-08 20:13:21 +0800

    Initial Commit

---
+++ GamePai/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GamePai
+//
+//  Created by student on 2018/11/8.
+//  Copyright © 2018年 xiaoshuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
